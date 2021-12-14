452a31707a1cf3316e980a2ea74e4255f4f2b6d8
AsterixxxGallier <asterixxxgallier@aol.com>
2021-12-14 22:52:54 +0100

    Rename BlockVenerableStump3x1x3 to BlockVenerableStump and remove the old, single-block BlockVenerableStump

--- src/main/java/com/github/alexthe666/iceandfire/block/BlockVenerableStump.java
+++ src/main/java/com/github/alexthe666/iceandfire/block/BlockVenerableStump.java
@@ -4,15 +4,22 @@ import com.github.alexthe666.iceandfire.IceAndFire;
 import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.PropertyEnum;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.IStringSerializable;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3i;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
 import javax.annotation.Nonnull;
 
 public class BlockVenerableStump extends Block {
+    public static final PropertyEnum<BlockVenerableStump.StumpPart> PART =
+            PropertyEnum.create("part", BlockVenerableStump.StumpPart.class);
+
+    // FIXME make dependent on BlockState.part
     @SuppressWarnings("PointlessArithmeticExpression")
     static AxisAlignedBB boundingBox = new AxisAlignedBB(
             -16.0 / 16.0,
@@ -68,5 +75,38 @@ public class BlockVenerableStump extends Block {
     @Override
     public AxisAlignedBB getSelectedBoundingBox(IBlockState state, World worldIn, BlockPos pos) {
         return boundingBox;
+    }
+
+    enum StumpPart implements IStringSerializable {
+        // --
+        NORTH_WEST(new Vec3i(-1, 0, -1)),
+        // -0
+        WEST(new Vec3i(-1, 0, 0)),
+        // -+
+        SOUTH_WEST(new Vec3i(-1, 0, +1)),
+        // 0-
+        NORTH(new Vec3i(0, 0, -1)),
+        // 00
+        MIDDLE(new Vec3i(0, 0, 0)),
+        // 0+
+        SOUTH(new Vec3i(0, 0, +1)),
+        // +-
+        NORTH_EAST(new Vec3i(+1, 0, -1)),
+        // +0
+        EAST(new Vec3i(+1, 0, 0)),
+        // ++
+        SOUTH_EAST(new Vec3i(+1, 0, +1));
+
+        Vec3i fromCenter;
+
+        StumpPart(Vec3i fromCenter) {
+            this.fromCenter = fromCenter;
+        }
+
+        @Override
+        @Nonnull
+        public String getName() {
+            return this.toString().toLowerCase();
+        }
     }
 }
