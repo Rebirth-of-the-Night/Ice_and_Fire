09f069343c8152f097c3dd64d4a328142a2b7d09
jchung01 <jjchung276@gmail.com>
2024-09-18 23:08:10 -0700

    Fix TOP dragon info

--- src/main/java/com/github/alexthe666/iceandfire/compat/theoneprobe/DragonInfoProvider.java
+++ src/main/java/com/github/alexthe666/iceandfire/compat/theoneprobe/DragonInfoProvider.java
@@ -5,10 +5,10 @@ import mcjty.theoneprobe.api.IProbeHitEntityData;
 import mcjty.theoneprobe.api.IProbeInfo;
 import mcjty.theoneprobe.api.IProbeInfoEntityProvider;
 import mcjty.theoneprobe.api.ProbeMode;
+
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.world.World;
-import slimeknights.tconstruct.library.Util;
 
 public class DragonInfoProvider implements IProbeInfoEntityProvider {
 
@@ -23,12 +23,16 @@ public class DragonInfoProvider implements IProbeInfoEntityProvider {
             EntityDragonBase dragon = (EntityDragonBase) entity;
 
             if (dragon.isTamed() && dragon.getOwner() != null) {
-                probeInfo.horizontal().text(Util.translateFormatted("dragon.owner") + dragon.getOwner().getName());
+                probeInfo.text(localize("dragon.owner") + " " + dragon.getOwner().getName());
             } else {
-                probeInfo.horizontal().text(Util.translateFormatted("dragon.untamed"));
+                probeInfo.text(localize("dragon.untamed"));
             }
-            probeInfo.horizontal().text(Util.translateFormatted("dragon.gender") + Util.translateFormatted(dragon.isMale() ? "dragon.gender.male" : "dragon.gender.female"));
-            probeInfo.horizontal().text(Util.translateFormatted("dragon.stage") + dragon.getDragonStage());
+            probeInfo.text(localize("dragon.gender") + " " + localize(dragon.isMale() ? "dragon.gender.male" : "dragon.gender.female"));
+            probeInfo.text(localize("dragon.stage") + " " + dragon.getDragonStage());
         }
+    }
+
+    private String localize(String text) {
+        return IProbeInfo.STARTLOC + text + IProbeInfo.ENDLOC;
     }
 }
